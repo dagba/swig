21c363d74bc4e2cb1a656b181f77c95b63bbabd9
Aleksey <akthesnipe@gmail.com>
2018-05-11 10:08:49 +0500

    Thread management in SWRingBack

--- Pod/Classes/Call/SWRingback.m
+++ Pod/Classes/Call/SWRingback.m
@@ -34,76 +34,120 @@
     
     SWEndpoint *endpoint = [SWEndpoint sharedEndpoint];
     
-    pj_status_t status;
+    NSThread *callThread = [endpoint.threadFactory getCallManagementThread];
     
-    pjmedia_tone_desc tone[kSWRingbackCount];
-    pj_str_t name = pj_str("tone");
+    __block BOOL shouldReturnNil = NO;
     
-    //TODO make ptime and channel count not constant?
-
-    NSUInteger samplesPerFrame = (kSWAudioFramePtime * endpoint.endpointConfiguration.clockRate * kSWChannelCount) / 1000;
+    __block pjmedia_port *ringbackPort;
+    __block NSInteger ringbackSlot;
     
-    pj_pool_t *pool = [endpoint pjPool];
+    [endpoint.threadFactory runBlock:^{
+        pj_status_t status;
+        
+        pjmedia_tone_desc tone[kSWRingbackCount];
+        pj_str_t name = pj_str("tone");
+        
+        //TODO make ptime and channel count not constant?
+        
+        NSUInteger samplesPerFrame = (kSWAudioFramePtime * endpoint.endpointConfiguration.clockRate * kSWChannelCount) / 1000;
+        
+        pj_pool_t *pool = [endpoint pjPool];
+        
+        status = pjmedia_tonegen_create2(pool, &name, (unsigned int)endpoint.endpointConfiguration.clockRate, kSWChannelCount, (unsigned int)samplesPerFrame, 16, PJMEDIA_TONEGEN_LOOP, &ringbackPort);
+        
+        if (status != PJ_SUCCESS) {
+            DDLogDebug(@"Error creating ringback tones");
+            shouldReturnNil = YES;
+            return;
+        }
+        
+        pj_bzero(&tone, sizeof(tone));
+        
+        for (int i = 0; i < kSWRingbackCount; ++i) {
+            tone[i].freq1 = kSWRingbackFrequency1;
+            tone[i].freq2 = kSWRingbackFrequency2;
+            tone[i].on_msec = kSWRingbackOnDuration;
+            tone[i].off_msec = kSWRingbackOffDuration;
+        }
+        
+        tone[kSWRingbackCount - 1].off_msec = kSWRingbackInterval;
+        
+        pjmedia_tonegen_play(ringbackPort, kSWRingbackCount, tone, PJMEDIA_TONEGEN_LOOP);
+        
+        status = pjsua_conf_add_port([endpoint pjPool], ringbackPort, (int *)&ringbackSlot);
+        
+        if (status != PJ_SUCCESS) {
+            DDLogDebug(@"Error adding media port for ringback tones");
+            shouldReturnNil = YES;
+            return;
+        }
+    } onThread:callThread wait:YES];
     
-    status = pjmedia_tonegen_create2(pool, &name, (unsigned int)endpoint.endpointConfiguration.clockRate, kSWChannelCount, (unsigned int)samplesPerFrame, 16, PJMEDIA_TONEGEN_LOOP, &_ringbackPort);
-    
-    if (status != PJ_SUCCESS) {
-        DDLogDebug(@"Error creating ringback tones");
+    if (shouldReturnNil) {
         return nil;
     }
-    
-    pj_bzero(&tone, sizeof(tone));
-    
-    for (int i = 0; i < kSWRingbackCount; ++i) {
-        tone[i].freq1 = kSWRingbackFrequency1;
-        tone[i].freq2 = kSWRingbackFrequency2;
-        tone[i].on_msec = kSWRingbackOnDuration;
-        tone[i].off_msec = kSWRingbackOffDuration;
+    else {
+        _ringbackPort = ringbackPort;
+        _ringbackSlot = ringbackSlot;
+        
+        return self;
     }
-    
-    tone[kSWRingbackCount - 1].off_msec = kSWRingbackInterval;
-    
-    pjmedia_tonegen_play(self.ringbackPort, kSWRingbackCount, tone, PJMEDIA_TONEGEN_LOOP);
-    
-    status = pjsua_conf_add_port([endpoint pjPool], [self ringbackPort], (int *)&_ringbackSlot);
-    
-    if (status != PJ_SUCCESS) {
-        DDLogDebug(@"Error adding media port for ringback tones");
-        return nil;
-    }
-    
-    return self;
 }
 
 -(void)dealloc {
-    int ringbackSlot = (int)_ringbackSlot;
-    pjmedia_port *ringbackPort = _ringbackPort;
     
     SWEndpoint *endpoint = [SWEndpoint sharedEndpoint];
     
     NSThread *callThread = [endpoint.threadFactory getCallManagementThread];
     
     [endpoint.threadFactory runBlock:^{
+        //Если либа начала перезагрузку, ничего делать уже не надо
+        if (pjsua_get_state() != PJSUA_STATE_RUNNING) {
+            return;
+        }
+        
+        int ringbackSlot = (int)self.ringbackSlot;
+        pjmedia_port *ringbackPort = self.ringbackPort;
+        
         pjsua_conf_remove_port(ringbackSlot);
         pjmedia_port_destroy(ringbackPort);
-    } onThread:callThread wait:NO];
+        
+    } onThread:callThread wait:YES];
     
     /*
-#warning main thread!
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        pjsua_conf_remove_port(ringbackSlot);
-        pjmedia_port_destroy(ringbackPort);
-    });
+     #warning main thread!
+     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+     pjsua_conf_remove_port(ringbackSlot);
+     pjmedia_port_destroy(ringbackPort);
+     });
      */
 }
 
 -(void)start {
-   pjsua_conf_connect((int)self.ringbackSlot, 0);
+    SWEndpoint *endpoint = [SWEndpoint sharedEndpoint];
+    
+    NSThread *callThread = [endpoint.threadFactory getCallManagementThread];
+    
+    __weak typeof(self) weakSelf = self;
+    [endpoint.threadFactory runBlock:^{
+        pjsua_conf_connect((int)weakSelf.ringbackSlot, 0);
+    } onThread:callThread wait:YES];
 }
 
 -(void)stop {
-    pjsua_conf_disconnect((int)self.ringbackSlot, 0);
-    pjmedia_tonegen_rewind(self.ringbackPort);
+    SWEndpoint *endpoint = [SWEndpoint sharedEndpoint];
+    
+    NSThread *callThread = [endpoint.threadFactory getCallManagementThread];
+    
+    __weak typeof(self) weakSelf = self;
+    [endpoint.threadFactory runBlock:^{
+        if (pjsua_get_state() != PJSUA_STATE_RUNNING) {
+            return;
+        }
+        
+        pjsua_conf_disconnect((int)weakSelf.ringbackSlot, 0);
+        pjmedia_tonegen_rewind(weakSelf.ringbackPort);
+    } onThread:callThread wait:YES];
 }
 
 @end
